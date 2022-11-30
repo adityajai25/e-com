fedfc0ecbfa31707e491dd43a3d894b5114e0034
adityajai25 <adityajai243@gmail.com>
2022-11-30 11:46:18 +0530

    added alert

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-
+  @Input() product!: Product;
 }
