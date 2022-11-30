a8680d9415e1d3ee216877040ae14b2cb19d9c8b
adityajai25 <adityajai243@gmail.com>
2022-11-30 11:56:53 +0530

    notify

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,5 +7,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
