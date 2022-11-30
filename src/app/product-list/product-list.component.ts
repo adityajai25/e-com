77401abd4617859c9a5275531e0f8120eea03bf2
adityajai25 <adityajai243@gmail.com>
2022-11-30 11:59:15 +0530

    completed

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,12 +8,18 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+
+  products = [...products];
 
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
+
 
 
 /*
