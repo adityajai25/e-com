2517d9b7ebcccafd5051d7e8731b0c7d97a16bc9
adityajai25 <adityajai243@gmail.com>
2022-12-13 11:22:31 +0530

    added price

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+}
